aa5e0f7de3196ef63fd3a942b5d8dcfafb432d53
xornivore <30147836+xornivore@users.noreply.github.com>
2025-11-05 13:48:10 -0500

    chore: rm -rf all to reset publishing from stainless

--- langsmith-java-core/src/test/kotlin/com/langsmith/api/client/LangSmithClientTest.kt
+++
@@ -1,5 +0,0 @@
-// File generated from our OpenAPI spec by Stainless.
-
-package com.langsmith.api.client
-
-class LangSmithClientTest
--- settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-rootProject.name = "langsmith-java-root"
-
-include("langsmith-java")
-include("langsmith-java-client-okhttp")
-include("langsmith-java-core")
-include("langsmith-java-example")
-include("langsmith-java-trace")
-include("examples")
