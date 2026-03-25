43616842f85d158a60ec6fd2cd2797d9f123a777
jacoblee93 <jacoblee93@gmail.com>
2026-03-25 09:41:34 -0700

    Fix error messages

--- langsmith-java-core/src/main/kotlin/com/langchain/smith/errors/UnauthorizedException.kt
+++ langsmith-java-core/src/main/kotlin/com/langchain/smith/errors/UnauthorizedException.kt
@@ -10,7 +10,7 @@ import kotlin.jvm.optionals.getOrNull
 
 class UnauthorizedException
 private constructor(private val headers: Headers, private val body: JsonValue, cause: Throwable?) :
-    LangChainServiceException("401: $body", cause) {
+    LangChainServiceException(formatMessage(401, body), cause) {
 
     override fun statusCode(): Int = 401
 
